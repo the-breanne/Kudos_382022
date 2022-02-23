8e938373fea6e6362b5a2826eddf7787210e2cc6
Breanne Beutler <breannebeutler@unomaha.edu>
2022-02-23 17:05:38 -0600

    Update Dashboard.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,4 +1,40 @@
-<div>
-  <h2>Dashboard</h2>
-  <p>This page is only visible to users that are currently logged in</p>
-</div>
+<template>
+    <div class="card">
+        <h5 class="card-header">Dashboard</h5>
+        <div class="card-body">
+            <h5 class="card-title">{{user.displayName}}</h5>
+            <p class="card-text">{{user.email}}</p>
+            <a href="#" class="btn btn-primary" @click="signOut()">Signout</a>
+        </div>
+    </div>
+</template>
+
+<script>
+import { db } from '../firebaseDb';
+
+export default {
+  data() {
+    return {
+      user: ''
+    };
+  },
+  created() {
+    db.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = '';
+      }
+    });
+  },
+  methods: {
+    signOut() {
+      db.auth().signOut().then(() => {
+        db.auth().onAuthStateChanged(() => {
+          this.$router.push('/home')
+        })
+      })
+    }
+  }
+};
+</script>
