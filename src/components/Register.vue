8aeb428c6770eae41f512b73f558633f2f6e2f41
Breanne Beutler <breannebeutler@unomaha.edu>
2022-02-23 10:50:20 -0600

    Update Register.vue

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -18,25 +18,25 @@
 </template>
 
 <script>
-        data() {
-            return { 
-                email: '', 
-                password: '', 
-            };
-        },
-        methods: {
-            register() {
-                firebase
-                .auth()
-                .createUserWithEmailAndPassword(this.email, this.password)
-                then(() => {
-                    alert('Successfully registered! Please login.');
-                    this.$router.push('/')
-                })
-            .catch(error => {
-                alert(error.message);
-            });
-        },
-        },
+data() { 
+  return { 
+    email: '', 
+    password: '', 
+  }; 
+},
+methods: {
+  register() {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.email, this.password)
+      then(() => {
+        alert('Successfully registered! Please login.');
+        this.$router.push('/');
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  },
+},
     }
 </script>
